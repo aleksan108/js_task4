44042a402bf83ad87cb5497f8183816a4201b731
git_zalex <alesia.za@gmail.com>
2022-09-16 21:33:18 +0300

    Small changes

--- scripts.js
+++ scripts.js
@@ -1,29 +1,32 @@
+
 function sum(x,y){
-    let z = Number(x) + Number(y);
-    let result = document.getElementById('result');
-    result.innerText="Итого: " + z;
+    const z = Number(x) + Number(y);
+    const result = document.getElementById('result');
+    result.textContent=`Итого:  ${z}`;
 }
 
 function deduction(x,y){
-    let z = Number(x) - Number(y);
-    let result = document.getElementById('result');
-    result.innerText="Итого: " + z;
+    const z = Number(x) - Number(y);
+    const result = document.getElementById('result');
+    result.textContent=`Итого:  ${z}`;
 }
 function multi(x,y){
-    let z = Number(x) * Number(y);
-    let result = document.getElementById('result');
-    result.innerText="Итого: " + z;
+    const z = Number(x) * Number(y);
+    const result = document.getElementById('result');
+    result.textContent=`Итого:  ${z}`;
 }
 function division(x,y){
-    let result = document.getElementById('result');
-    if (Number(y)===0){
-        result.innerText="Делить на 0?? No way";
+    const result = document.getElementById('result');
+    if (!Number(y)){
+        result.textContent="Делить на 0?? No way";
     } else{
-        let z = Number(x) / Number(y);
-        result.innerText="Итого: " + z;
+        const z = Number(x) / Number(y);
+        result.textContent=`Итого:  ${z}`;
     }
 }
+
+const formID = document.getElementById('formID');
+
 function changeColor(){
-    let formID = document.getElementById('formID');
     formID.style.backgroundColor='pink';
 }
